82e8281097989a48ee55ec8a75a956bb6b7b852a
Valentin Karakashev <116033694+ValchoPalcho@users.noreply.github.com>
2024-04-13 13:51:25 +0300

    Add project files.

---
+++ Operations-Research/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Operations_Research
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
